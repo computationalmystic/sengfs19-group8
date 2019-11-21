cb3d5baf4f58f58690c3befb1162028a052e32ea
Claire Hough <claire.n.hough@gmail.com>
2019-11-20 21:05:56 -0600

    added drop down menus for two pages

--- SweFinalProject/src/app/compare-contributors/compare-contributors.component.ts
+++ SweFinalProject/src/app/compare-contributors/compare-contributors.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-compare-contributors',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compare-contributors.component.css']
 })
 export class CompareContributorsComponent implements OnInit {
+  repoChoiceForm = this.fb.group({
+    choice1: ['', Validators.compose([Validators.required])],
+    choice2: ['', Validators.compose([Validators.required])]
+    // , Validators.pattern('^[a-zA-Z0-9s-]*$')
+  });
 
-  constructor() { }
+  repos: Object;
+
+  constructor(private route: ActivatedRoute, private dataService: DataService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    console.log("getRepos");
+    this.dataService.getRepos().subscribe(data => {
+      this.repos = data;
+      console.log(this.repos);
+    });
   }
 
+  submitChoice() {
+
+  }
 }
--- SweFinalProject/src/app/compare-issues/compare-issues.component.ts
+++ SweFinalProject/src/app/compare-issues/compare-issues.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-compare-issues',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompareIssuesComponent implements OnInit {
 
-  constructor() { }
+  repoChoiceForm = this.fb.group({
+    choice1: ['', Validators.compose([Validators.required])],
+    choice2: ['', Validators.compose([Validators.required])]
+    // , Validators.pattern('^[a-zA-Z0-9s-]*$')
+  });
+
+  repos: Object;
+
+  constructor(private route: ActivatedRoute, private dataService: DataService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    console.log("getRepos");
+      this.dataService.getRepos().subscribe(data => {
+        this.repos = data;
+        console.log(this.repos);
+      });
   }
 
+  submitChoice(){
+    
+  }
 }
